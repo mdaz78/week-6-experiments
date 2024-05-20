5b5c7b4c79323a29185eb89f2706db61b67e8018
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-20 14:55:35 +0530

    chore: Refactor Todo component to fetch and display todo items from API

--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const AddToDo = () => {
   return <div>AddToDo</div>;
 };
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,44 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoItem from "./TodoItem";
 
-const Todo = () => {
-  const [todoItems, setTodoItems] = useState([
-    {
-      id: 1,
-      title: "Learn React",
-      description: "learn react from kirat",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: "Learn Node",
-      description: "learn node from kirat",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: "Learn Express",
-      description: "learn express from kirat",
-      isCompleted: false,
-    },
-  ]);
+const API_URL = "https://sum-server.100xdevs.com/todos";
 
-  const addTodo = () => {
-    setTodoItems([
-      {
-        id: todoItems.length + 1,
-        title: "some title",
-        description: "some description",
-        isCompleted: false,
-      },
-      ...todoItems,
-    ]);
-  };
+const Todo = () => {
+  const [todoItems, setTodoItems] = useState([]);
+
+  useEffect(() => {
+    const getTodoItems = async () => {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setTodoItems(data.todos);
+    };
+
+    const intervalId = setInterval(getTodoItems, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div>
-      <button onClick={addTodo}>Add New to do</button>
       <div className="space-y-2 space-x-2">
+        {console.log(todoItems)}
         {todoItems.map((item) => (
           <TodoItem key={item.id} {...item} />
         ))}
